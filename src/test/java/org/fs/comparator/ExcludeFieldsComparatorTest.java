681e8a3ca209c296ef6f83738e69bf8dae298f83
fs <grag656@gmail.com>
2025-05-18 01:35:24 +0500

    Parametrized tests

--- src/test/java/org/fs/comparator/ExcludeFieldsComparatorTest.java
+++ src/test/java/org/fs/comparator/ExcludeFieldsComparatorTest.java
@@ -1,184 +1,126 @@
 package org.fs.comparator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SuppressWarnings("unused")
 public class ExcludeFieldsComparatorTest {
-    @Test
-    @DisplayName("Two equals fields")
-    void test1() {
-        class Left {
-            private String one;
-            private String two;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
 
-        class Right {
-            private String two;
-            private String three;
-        }
-        var right = new Right();
-        right.two = "12";
-        right.three = "123";
-
-        assertThat(
-            ComparatorUtils.compareObjects(left, right)
+    @ParameterizedTest
+    @DisplayName("Simple comparator")
+    @MethodSource("compareFirstFields")
+    void test1(boolean expected, Object left, Object right) {
+        boolean actual = ComparatorUtils.compareObjects(left, right)
                 .compareMatchingNames()
-                .compare()
-        ).isTrue();
+                .compare();
+
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("Two not equals fields")
-    void test2() {
-        class Left {
-            private String one;
-            private String two;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
+    @ParameterizedTest
+    @DisplayName("Exclude fields")
+    @MethodSource("compareWithExclude")
+    void test2(boolean expected, String[] exclude, Object left, Object right) {
+        boolean actual = ComparatorUtils.compareObjects(left, right)
+                .compareMatchingNames()
+                .excludeFields(exclude)
+                .compare();
 
-        class Right {
-            private String two;
-            private String three;
-        }
-        var right = new Right();
-        right.two = "12 Incorrect";
-        right.three = "123";
-
-        assertThat(
-                ComparatorUtils.compareObjects(left, right)
-                        .compareMatchingNames()
-                        .compare()
-        ).isFalse();
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("Default not equals")
-    void test3() {
-        class Left {
-            private String one;
-            private String two;
-            private String three;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
-        left.three = "123";
-
-        class Right {
-            private String two;
-            private String three;
-        }
-        var right = new Right();
-        right.two = "12 Incorrect";
-        right.three = "123";
-
-        assertThat(
-                ComparatorUtils.compareObjects(left, right)
-                        .compareMatchingNames()
-                        .compare()
-        ).isFalse();
+    @SuppressWarnings("FieldMayBeFinal")
+    private static Stream<Arguments> compareFirstFields() {
+        return Stream.of(
+            Arguments.of(
+                true,
+                new Object() {
+                    private String one = "1";
+                    private String two = "12";
+                },
+                new Object() {
+                    private String two = "12";
+                    private String three = "123";
+                }
+            ),
+            Arguments.of(
+                false,
+                new Object() {
+                    private String one = "1";
+                    private String two = "12";
+                },
+                new Object() {
+                    private String two = "12 Incorrect";
+                    private String three = "123";
+                }
+            ),
+            Arguments.of(
+                false,
+                new Object() {
+                    private String one = "1";
+                    private String two = "12";
+                    private String three = "123";
+                },
+                new Object() {
+                    private String two = "12 Incorrect";
+                    private String three = "123";
+                }
+            )
+        );
     }
 
-    @Test
-    @DisplayName("Equals exclude field")
-    void test4() {
-        class Left {
-            private String one;
-            private String two;
-            private String three;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
-        left.three = "123";
-
-        class Right {
-            private String two;
-            private String three;
-        }
-        var right = new Right();
-        right.two = "12 Incorrect";
-        right.three = "123";
-
-        assertThat(
-                ComparatorUtils.compareObjects(left, right)
-                        .compareMatchingNames()
-                        .excludeFields("two")
-                        .compare()
-        ).isTrue();
-    }
-
-    @Test
-    @DisplayName("Equals exclude fields")
-    void test5() {
-        class Left {
-            private String one;
-            private String two;
-            private String three;
-            private String four;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
-        left.three = "123";
-        left.four = "1234";
-
-        class Right {
-            private String two;
-            private String three;
-            private String four;
-        }
-        var right = new Right();
-        right.two = "12 Incorrect";
-        right.three = "123";
-        right.four = "1234 Incorrect";
-
-        assertThat(
-                ComparatorUtils.compareObjects(left, right)
-                        .compareMatchingNames()
-                        .excludeFields("two", "four")
-                        .compare()
-        ).isTrue();
-    }
-
-    @Test
-    @DisplayName("Not equals exclude field")
-    void test6() {
-        class Left {
-            private String one;
-            private String two;
-            private String three;
-            private String four;
-        }
-        var left = new Left();
-        left.one = "1";
-        left.two = "12";
-        left.three = "123";
-        left.four = "1234";
-
-        class Right {
-            private String two;
-            private String three;
-            private String four;
-        }
-        var right = new Right();
-        right.two = "12 Incorrect";
-        right.three = "123";
-        right.four = "1234 Incorrect";
-
-        assertThat(
-                ComparatorUtils.compareObjects(left, right)
-                        .compareMatchingNames()
-                        .excludeFields("two")
-                        .compare()
-        ).isFalse();
+    @SuppressWarnings("FieldMayBeFinal")
+    private static Stream<Arguments> compareWithExclude() {
+        return Stream.of(
+                Arguments.of(
+                        true,
+                        new String[] { "two" },
+                        new Object() {
+                            private String one = "1";
+                            private String two = "12";
+                            private String three = "123";
+                        },
+                        new Object() {
+                            private String two = "12 Incorrect";
+                            private String three = "123";
+                        }
+                ),
+                Arguments.of(
+                        false,
+                        new String[] { "two" },
+                        new Object() {
+                            private String one = "1";
+                            private String two = "12";
+                            private String three = "123";
+                            private String four = "1234";
+                        },
+                        new Object() {
+                            private String two = "12 Incorrect";
+                            private String three = "123";
+                            private String four = "1234 Incorrect";
+                        }
+                ),
+                Arguments.of(
+                        false,
+                        new String[] { "two" },
+                        new Object() {
+                            private String one = "1";
+                            private String two = "12";
+                            private String three = "123";
+                            private String four = "1234";
+                        },
+                        new Object() {
+                            private String two = "12 Incorrect";
+                            private String three = "123";
+                            private String four = "1234 Incorrect";
+                        }
+                )
+        );
     }
 }
